93e0bd3e719040805934309825f987384b268101
leovujanic <leonard.vujanic@gmail.com>
2016-12-05 21:02:49 +0100

    PS-2016.3 <leonardvujanic@leonard-vujanic-macbook-pro.local Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, Eli_sDark.icls, .DS_Store, PHPfunctions.xml, project.default.xml, diff.xml, git.xml, other.xml, LA Model.php, AngularJS .xml, Twig.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, code.style.schemes.xml, colors.scheme.xml, Angular2.xml, databaseDrivers.xml, github_settings.xml, Notepad__.icls__2.icls, Default.xml, PHP.xml, LeoScheme.icls, HeatofTheNight.icls, laf.xml, Default _1_.xml, web-browsers.xml, vcs.xml, databaseSettings.xml, keymap.xml, Dark Peacock_2.icls, editor.xml, uml.xml, phpstorm-nature-theme.icls, webServers.xml, debugger.xml, MyGroup.xml

---
+++ fileTemplates/LA Model.php
@@ -0,0 +1,76 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+
+class ${NAME} {
+
+    # region Constants
+
+	# endregion
+
+	# region Variables
+
+	# endregion
+
+	# region Base
+
+	# endregion
+
+	# region Overrides
+
+	# endregion
+
+	# region Validations
+
+	# endregion
+
+	# region Events
+
+	# endregion
+
+	# region Relations
+
+	# endregion
+
+	# region Query modifications
+
+	# endregion
+
+	# region Finders
+
+	# endregion
+
+	# region Custom
+
+	# endregion
+
+	# region Templates, views, html and classes
+
+	# endregion
+
+	# region Urls
+
+	# endregion
+
+	# region Getters
+
+	# endregion
+
+	# region Setters
+
+	# endregion
+
+	# region Checkers
+
+	# endregion
+	
+	# region Cache stuff
+
+	# endregion
+
+}
